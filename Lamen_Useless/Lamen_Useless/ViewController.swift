2da82c20e3ac87dfd89fd766edb47b3407a4e0b5
Nathalia <nathaliamelare@gmail.com>
2020-04-16 18:48:30 -0300

    First commit

---
+++ Lamen_Useless/Lamen_Useless/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lamen_Useless
+//
+//  Created by Nathalia Melare on 16/04/20.
+//  Copyright © 2020 Nathalia Melare. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
