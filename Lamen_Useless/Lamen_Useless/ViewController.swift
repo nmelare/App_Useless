9d576aeecdf5694ddeabab745f24ef14f938e749
Nathalia <nathaliamelare@gmail.com>
2020-04-20 16:45:48 -0300

    Add motion manager

--- Lamen_Useless/Lamen_Useless/ViewController.swift
+++ Lamen_Useless/Lamen_Useless/ViewController.swift
@@ -13,17 +13,34 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var backgroundImage: UIImageView!
     
-    var lamenArray = UIImage.animatedImageNamed("Comendo", duration: 0.8)
+    @IBOutlet weak var bowlImage: UIImageView!
     
+    let lamenArray = UIImage.animatedImageNamed("Comendo", duration: 0.8)
+    
+    var tap: UITapGestureRecognizer!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
     }
     
+//    @objc func handleTap(_ gesture: UITapGestureRecognizer) {
+//         let tapLocation = gesture.location(in: backgroundImage.superview)
+//        if ((backgroundImage.layer.presentation()?.frame.contains(tapLocation)) != nil) {
+//
+//        }
+//    }
+
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        guard var lamenDuration = lamenArray?.duration else { return }
+//        lamenDuration += 0.1
         backgroundImage.image = lamenArray
+        
     }
 
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+              
+    }
+    
 }
 
